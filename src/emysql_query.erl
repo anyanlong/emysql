a14465fc67226b5b15dee7e1489926a5a56ded37
Jack Tang <jack@taodinet.com>
2014-04-29 21:35:43 +0800

    complete find and find_first

--- src/emysql_query.erl
+++ src/emysql_query.erl
@@ -9,7 +9,8 @@
 -module(emysql_query).
 
 %% API
--export([]).
+-export([find/2, find/4]).
+-export([find_first/4, find_each/6]).
 
 
 -include("emysql.hrl").
@@ -17,6 +18,44 @@
 %%%===================================================================
 %%% API
 %%%===================================================================
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% e.g: find(my_pool, ["SELECT name FROM users where age > ? and time > ?", 12, "2013-12-03" ])
+%% 
+%%
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
+find(ConnPool, [RawSql | Values]) ->
+    StmtName = type_utils:md5_hex(RawSql),
+    emysql:prepare(StmtName, type_utils:any_to_binay(RawSql)),
+    case emysql:execute(ConnPool, StmtName, Values) of
+        #ok_packet{} = OkPacket       -> OkPacket;
+        #error_packet{} = ErrorPacket -> ErrorPacket
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% e.g: find(my_pool, users, [ {select, [name, age]},
+%%                             {where,  ["age > ?", Age]},
+%%                             {order,  "id desc"},
+%%                             {limit,  100 }], ?AS_REC(user))
+%%
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
+find(ConnOrPool, Table, SqlOptions, [Rec, RecFields] = _AsRec)  ->
+    {Sql, CondVals} = build_sql(Table, SqlOptions),
+    Result = case ConnOrPool of
+                 #emysql_connection{} = Conn ->
+                     emysql_conn:execute(Conn, Sql, CondVals);
+                 Pool ->
+                     emysql:execute(Pool, Sql, CondVals)
+             end,
+    emysql_util:as_record(Result, Rec, RecFields).
+
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -34,76 +73,88 @@ find_first(ConnOrPool, Table, SqlOptions, AsRec) ->
 
 %%--------------------------------------------------------------------
 %% @doc
-%% e.g: find(my_pool, users, [ {select, [name, age]},
-%%                             {where,  ["age > ?", Age]},
-%%                             {order,  "id desc"},
-%%                             {limit,  100 }], ?AS_REC(user))
-%%
-%% @spec
-%% @end
-%%--------------------------------------------------------------------
-find(ConnOrPool, Table, SqlOptions, [Rec, RecFields] = _AsRec)  ->
-    {FindSql, CondVals} = build_sql(Table, SqlOptions),
-    Result = case ConnOrPool of
-                 #emysql_connection{} = Conn ->
-                     emysql_conn:execute(Conn, FindSql, CondVals);
-                 Pool ->
-                     emysql:execute(Pool, FindSql, CondVals)
-             end,
-    emysql_util:as_record(Result, Rec, RecFields).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% e.g: find(my_pool, ["SELECT name FROM users where age > ? and time > ?", 12, "2013-12-03" ])
-%% 
-%%
-%% @spec
-%% @end
-%%--------------------------------------------------------------------
-find(ConnPool, [RawSql | Values]) ->
-    StmtName = md5_hex(RawSql),
-    emysql:prepare(StmtName, type_utils:any_to_binay(RawSql)),
-    case emysql:execute(ConnPool, StmtName, Values) of
-        #ok_packet{} = OkPacket -> OkPacket;
-        #error_packet{} = ErrorPacket -> ErrorPacket
-    end.     
-
-%%--------------------------------------------------------------------
-%% @doc
 %%
 %% e.g:
 %%      Fun = fun(#user{} = U) ->
 %%                do_something_to(U)
 %%            end,
-%%      find(my_pool, users, [ {select, [name, age]},
+%%      find(my_pool, users, [ {select, ["name", "age"]},
 %%                             {where,  ["age > ?", Age]},
 %%                             {order,  "id desc"},
-%%                             {limit,  100 }], 10, ?AS_REC(user), Fun)
+%%                             {limit,  100 }            ], 10, ?AS_REC(user), Fun)
 %%
 %% @spec
 %% @end
 %%--------------------------------------------------------------------
-find_each(Connection, Table, SqlOptions, Size, AsRec, Fun) ->
-    ok.
+find_each(ConnOrPool, Table, SqlOptions, BatchSize, AsRec, Fun) ->
+    {FindSql, CountSql, CondVals} = build_sql(Table, SqlOptions),
+    Result = case ConnOrPool of
+                 #emysql_connection{} = Conn ->
+                     emysql_conn:execute(Conn, CountSql, CondVals);
+                 Pool ->
+                     emysql:execute(Pool, CountSql, CondVals)
+             end,
+    case Result of
+        #ok_packet{} ->
+            Total = 1000,
+            Limit = proplists:get_value(limit, SqlOptions, 0),
+            Remain = 10, % (Total > Limit ? Limit : Total),
+            do_find_each(ConnOrPool, Table, FindSql, CondVals, BatchSize, AsRec, Fun, Remain);
+        #error_packet{} ->
+            ok
+    end.
+    
 
+do_find_each(ConnOrPool, Table, Sql, CondVals, BatchSize, [Rec, RecFields] = AsRec, Fun, Remain) ->
+    Result = case ConnOrPool of
+                 #emysql_connection{} = Conn ->
+                     emysql_conn:execute(Conn, Sql, CondVals);
+                 Pool ->
+                     emysql:execute(Pool, Sql, CondVals)
+             end,
+    lists:foreach(
+      fun(Item) ->
+              Fun(emysql_util:as_record(Result, Rec, RecFields))
+      end, Result),
+    
+    case Remain - BatchSize > 0 of
+        true -> 
+            do_find_each(ConnOrPool, Table, Sql, CondVals, BatchSize, AsRec, Fun, (Remain - BatchSize));
+        false ->
+            ok
+    end.
+        
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 build_sql(Table, SqlOptions) ->
-    SelectF =  proplists:get_value(select, SqlOptions, "*"), 
+    SelectFields =
+        case proplists:get_value(select, SqlOptions) of
+            undefined         -> "*";
+            V when is_list(V) -> string:join(V, ",");
+            _ -> throw(bad_sql_select)
+        end,
         
-    Select = "SELECT " ++ SelectF ++ " FROM " ++ type_utils:any_to_list(Table),
     {Where, CondVals} = case proplists:get_value(where, SqlOptions) of
-                            undefined -> " ";
-                            [Cond | Values] -> {Cond, Values}
+                            undefined -> "";
+                            [Cond | Values] -> {"WHERE " ++ Cond, Values}
                         end,
-    FindSql = lists:append(Select, Where),
-    {FindSql, CondVals}.
+    
+    Order = case proplists:get_value(order, SqlOptions) of
+                undefined -> "";
+                OrderBy   -> " ORDER BY " ++ type_utils:any_to_list(OrderBy)
+            end,
+    Limit = case proplists:get_value(limit, SqlOptions) of
+                undefined   -> "";
+                [LV1, LV2]  ->
+                    " LIMIT " ++ type_utils:any_to_list(LV1) ++ ", " ++ type_utils:any_to_list(LV2);
+                LV3         -> " LIMIT " ++ type_utils:any_to_list(LV3)
+            end,
 
-
-md5_hex(S) ->
-    S.
-    % lists:flatten([io_lib:format("~.16b",[N]) || <<>> <= erlang:md5(S)]).
+    Table2 = type_utils:any_to_list(Table),
+    FindSql  = string:join(["SELECT", SelectFields, "FROM", Table2, Where, Order, Limit], " "),
+    CountSql = string:join(["SELECT COUNT(1) FROM", Table2, Where], " "),
+    {FindSql, CountSql, CondVals}.
                              
             
                     
