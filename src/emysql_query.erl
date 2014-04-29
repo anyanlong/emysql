5342929ab77152ee96884723d807776b14d89e89
Jack Tang <jack@taodinet.com>
2014-04-29 22:21:31 +0800

    refine find

--- src/emysql_query.erl
+++ src/emysql_query.erl
@@ -31,10 +31,7 @@
 find(ConnPool, [RawSql | Values]) ->
     StmtName = type_utils:any_to_atom("stmt_" ++ type_utils:md5_hex(RawSql)),
     emysql:prepare(StmtName, type_utils:any_to_binary(RawSql)),
-    case emysql:execute(ConnPool, StmtName, Values) of
-        #ok_packet{} = OkPacket       -> OkPacket;
-        #error_packet{} = ErrorPacket -> ErrorPacket
-    end.
+    emysql:execute(ConnPool, StmtName, Values).
 
 %%--------------------------------------------------------------------
 %% @doc
