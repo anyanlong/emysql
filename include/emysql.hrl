a36aa757ba3d2d8a73366fcae6854081c19877b3
jiangyanwei <yanwei@jiangyanweideMacBook-Pro.local>
2018-09-10 13:48:00 +0800

    update

--- include/emysql.hrl
+++ include/emysql.hrl
@@ -111,7 +111,7 @@
 			field_list :: list(),
 			rows, extra}).
 
--define(TIMEOUT, 8000).
+-define(TIMEOUT, 10000).
 -define(LOCK_TIMEOUT, 5000).
 -define(MAXPACKETBYTES, 50000000).
 -define(LONG_PASSWORD, 1).
